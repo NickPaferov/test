dc09313b9efe0d4cf12821642a13cf6e210c1a4f
Paferov Nikolai <paferov.nikolai@gmail.com>
2022-10-29 22:17:20 +0300

    feat: add component year

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
 import {Week} from './Week';
+import {Year} from "./Year";
 
 export const App = () => {
     return <>
         <Week/>
+        <Year/>
     </>
 }
